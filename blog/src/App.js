768ee4f4c698fd2ba7ed5f60318dfd2d3be12578
yura <rladbfk0000@naver.com>
2022-09-05 00:47:35 +0900

    Update

--- blog/src/App.js
+++ blog/src/App.js
@@ -7,67 +7,67 @@ import { useState } from 'react';
 function App() {
 
   let post= '강남 우동 맛집';
-  // 잠깐 저장할 땐 변수 사용 let, var ,const 
-  // state
   let [글제목,글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','파이썬 독학']);
-  let [따봉,따봉변경] = useState(0);
-
+  let [따봉,따봉변경] = useState([0,0,0]);
+  let [modal,setModal]=  useState(false);
+  // function 안의 것을 array 자료 개수만큼 함수안의 코드 실행
   
-
+  
+  
   return (
     <div className="App">
       <div className="black-nav">
         <h4>React Blog</h4>
         </div>
 
-        <button onClick={()=>{
-          let copy =[...글제목];
-          copy.sort();
-          글제목변경(copy)
-        }}>가나다순 정렬</button>
+    {/* <div className='list'>
+      <h4>{ 글제목[0]} <span onClick={()=>{따봉변경(따봉+1)}}>👍
+      </span>{따봉}</h4>
+    </div>
+    <div className='list'>
+      <h4>{글제목[1]}</h4>
+      <p>2월 17일 발행</p>
+    </div>
+    <div className='list'>
+      <h4 onClick={()=>{setModal(true)}}>{글제목[2]}</h4>
+     <p>2월 17일 발행</p>
+    </div> */}
 
-          <button onClick={()=>{
-            let copy= [...글제목];
-            copy[0]='여자 코트 추천';
-            글제목변경(copy);
-          }}>글 수정</button>
+{
+  글제목.map(function(a,i){
+    return (
+      <div className='list'>
+      <h4>{글제목[i]}
+      <span onClick={()=>{
+        let copy = [...따봉];
+        copy[i]=copy[i]+1;
+        따봉변경(copy)}}>👍
+      </span>{따봉[i]}</h4>
+      <p>2월 17일 발행</p>
+    </div>
+    )
+  })
+}
 
-        <div className='list'>
-          <h4>{글제목[0]} <span onClick={()=>{따봉변경(따봉+1)}}>💝</span>{따봉}</h4>
-          <p>2월 17일 발행</p>
-        </div>
-        <div className='list'>
-          <h4>{글제목[1]}</h4>
-          <p>2월 17일 발행</p>
-        </div>
-        <div className='list'>
-          <h4>{글제목[2]}</h4>
-          <p>2월 17일 발행</p>
-        </div>
+         {
+        modal ==true? <Modal color={'skyblue'} 글제목={글제목}/>: null 
+       }
+   
 
-       <Modal></Modal>
-       <Modal2></Modal2>
         </div>
   );
 }
-function Modal() {
+function Modal(props) {
   return (
-    <div className='modal'>
-          <h4>제목</h4>
+    <div className='modal' style={{background:props.color}}>
+          <h4>{props.글제목[0]}</h4>
           <p>날짜</p>
           <p>상세내용</p>
+          <button className='글제목'>글 수정</button>
         </div>
   )
 }
 
-function Modal2() {
-  return (
-    <div className='modal'>
-      <h4>댓글</h4>
-      <p>작성자</p>
-      <p>댓글 내용</p>
-    </div>
-  )
-}
+
 
 export default App;
