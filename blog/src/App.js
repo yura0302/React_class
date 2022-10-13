1a73152148bce2dd3c27d1961458100500e9c2dc
yura <rladbfk0000@naver.com>
2022-10-14 02:27:14 +0900

    chore : useState

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
   let post = "강남 우동 맛집";
+  let [a, b] = useState(["남자 코트 추천", "강남 우동맛집", "파이썬 독학"]);
+
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{ color: "red", fontSize: "16px" }}>블로그임</h4>
+        <h4>React Blog</h4>
       </div>
-      <h4>{post}</h4>
-      <h4>{post}</h4>
-      <h4>{post}</h4>
+      <div className="list">
+        <h4>{a[0]}</h4>
+        <p>10월 14일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{a[1]}</h4>
+        <p>10월 14일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{a[2]}</h4>
+        <p>10월 14일 발행</p>
+      </div>
     </div>
   );
 }
