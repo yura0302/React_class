4d573eba75a13bbad291d891b6b204e1ebf748d5
yura <rladbfk0000@naver.com>
2022-10-19 23:51:05 +0900

    etc : 과제

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,9 +1,16 @@
-import { useState } from "react";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   let post = "강남 우동 맛집";
-  let [a, b] = useState(["남자 코트 추천", "강남 우동맛집", "파이썬 독학"]);
+  let [글제목, b] = useState([
+    "남자 코트 추천",
+    "강남 우동맛집",
+    "파이썬 독학",
+  ]);
+
+
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
@@ -11,15 +18,18 @@ function App() {
         <h4>React Blog</h4>
       </div>
       <div className="list">
-        <h4>{a[0]}</h4>
+        <h4>
+          {글제목[0]} <span onClick={함수}>❤️‍🔥</span>
+          {따봉}
+        </h4>
         <p>10월 14일 발행</p>
       </div>
       <div className="list">
-        <h4>{a[1]}</h4>
+        <h4>{글제목[1]}</h4>
         <p>10월 14일 발행</p>
       </div>
       <div className="list">
-        <h4>{a[2]}</h4>
+        <h4>{글제목[2]}</h4>
         <p>10월 14일 발행</p>
       </div>
     </div>
